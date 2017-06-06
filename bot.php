36d9f1fa812728f1f1e800b28e6b35a61216e6c6
phoure <phoure.capuchino@msn.com>
2017-06-07 03:16:03 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 $access_token = '1n4HF8OIC9v65ocWyJAtnzMOUSyiZf6rrP1/xLKQDtFK+nKupweT4dVMBFP79mgVgC35CsJzx3pYOgRFBp7kodhi2d8/tXR1Ked59ISLLlz4yLxNohKdBMuHKnN0odSaT0iZ0ie7ObmpjYh8+jjHUwdB04t89/1O/w1cDnyilFU=';
 
 
-echo '55+++sdfsssvvvvs65';
+echo '1111111';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -35,20 +35,20 @@ if (!is_null($events['events'])) {
 		'template' => array(
 			'type' => 'confirm',
 			'text' => 'Are you sure?',
-			'actions' => array(
-				[
+			'actions' => [
+				array(
 					'type' => 'postback',
 					'label' => 'Yes',
 					'data' => 'btnyes',
 					'text' => 'yes'
-				],
-				[
+				),
+				array(
 					'type' => 'postback',
 					'label' => 'No',
 					'data' => 'btnno',
 					'text' => 'no'
-				]
-			)
+				)
+			]
 		)
 	)
 );
