3a3c87f088bfd8957ac4e59c511086ed95a8e29e
phoure <phoure.capuchino@msn.com>
2017-06-07 04:12:02 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 $access_token = '1n4HF8OIC9v65ocWyJAtnzMOUSyiZf6rrP1/xLKQDtFK+nKupweT4dVMBFP79mgVgC35CsJzx3pYOgRFBp7kodhi2d8/tXR1Ked59ISLLlz4yLxNohKdBMuHKnN0odSaT0iZ0ie7ObmpjYh8+jjHUwdB04t89/1O/w1cDnyilFU=';
 
 
-echo 'หกด';
+echo 'หกดฟดหกดหกด';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -22,29 +22,40 @@ if (!is_null($events['events'])) {
 
 
 
-	$sss = array(
-	'replyToken' => $replyToken,
-	'messages' => [[
-		'type' => 'template',
-		'altText' => 'this is a confirm template',
-		'template' => [[
-			'type' => 'confirm',
-			'text' => 'Are you sure?',
-			'actions' => array(
-				[[
+		$a1 = [
 					'type' => 'message',
 					'label' => 'Yes',
 					'text' => 'yes'
-				]],
-				[[
+				];
+	$a2 = [
 					'type' => 'message',
 					'label' => 'No',
 					'text' => 'no'
-				]]
-			)
-		]]
-	]]
-);
+				];			
+				
+$vvv =  array($a1, $a2);
+
+
+
+		$oo =[
+		'type' => 'confirm',
+			'text' => 'Are you sure?',
+			'actions' => $vvv
+			];
+			
+			
+	$mes = [
+		'type' => 'template',
+		'altText' => 'this is a confirm template',
+		'template' => [$oo]
+		]
+		
+		;
+		
+	$sss = array(
+	'replyToken' => $replyToken,
+	'messages' => [$mes]
+	);
 					
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
