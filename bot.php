abeed40256d6e955056029f9ae30493344acded4
phoure <phoure.capuchino@msn.com>
2017-06-07 04:43:31 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 $access_token = '1n4HF8OIC9v65ocWyJAtnzMOUSyiZf6rrP1/xLKQDtFK+nKupweT4dVMBFP79mgVgC35CsJzx3pYOgRFBp7kodhi2d8/tXR1Ked59ISLLlz4yLxNohKdBMuHKnN0odSaT0iZ0ie7ObmpjYh8+jjHUwdB04t89/1O/w1cDnyilFU=';
 
 
-echo 'eeesdafasdfeee';
+echo '555555555';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -38,6 +38,13 @@ $aa = array([
 		'type' => 'sticker',
 		'packageId' => '1',
 		'stickerId' => '1',
+	],
+	[
+		'type' => 'location',
+    "title"=> "my location",
+    "address"=> "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
+    "latitude"=> 35.65910807942215,
+    "longitude"=> 139.70372892916203,
 	]
 	);
 
