d6e59a69622c5fcfc30d7c27ff7331228d672aad
phoure <phoure.capuchino@msn.com>
2017-06-07 02:34:34 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 $access_token = '1n4HF8OIC9v65ocWyJAtnzMOUSyiZf6rrP1/xLKQDtFK+nKupweT4dVMBFP79mgVgC35CsJzx3pYOgRFBp7kodhi2d8/tXR1Ked59ISLLlz4yLxNohKdBMuHKnN0odSaT0iZ0ie7ObmpjYh8+jjHUwdB04t89/1O/w1cDnyilFU=';
 
 
-echo 'eee';
+echo 'esee';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -20,13 +20,13 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 			$id = $event['source']['userId'];
 
-$sss = array(
+$sss = [
 	'replyToken' => $replyToken,
-	'messages' => array(
+	'messages' => [
 		'type' => 'text',
 		'text' => 'this is a confirm template'
-	)
-);
+	]
+];
 
 			// Build message to reply back
 			$messages = [
