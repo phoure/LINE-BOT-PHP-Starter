509a3664a30cb10316046ed0a62d30226ebec17c
phoure <phoure.capuchino@msn.com>
2017-06-07 04:15:42 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 $access_token = '1n4HF8OIC9v65ocWyJAtnzMOUSyiZf6rrP1/xLKQDtFK+nKupweT4dVMBFP79mgVgC35CsJzx3pYOgRFBp7kodhi2d8/tXR1Ked59ISLLlz4yLxNohKdBMuHKnN0odSaT0iZ0ie7ObmpjYh8+jjHUwdB04t89/1O/w1cDnyilFU=';
 
 
-echo 'หกดฟดหกดหกด';
+echo 'sdfsasdf';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -40,7 +40,7 @@ $vvv =  array($a1, $a2);
 		$oo =[
 		'type' => 'confirm',
 			'text' => 'Are you sure?',
-			'actions' => $vvv
+			'actions' => [$vvv]
 			];
 			
 			
