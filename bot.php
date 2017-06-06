2521301ac6c32518d7977023210cf1f1cf4a9c83
phoure <phoure.capuchino@msn.com>
2017-06-07 04:18:51 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 $access_token = '1n4HF8OIC9v65ocWyJAtnzMOUSyiZf6rrP1/xLKQDtFK+nKupweT4dVMBFP79mgVgC35CsJzx3pYOgRFBp7kodhi2d8/tXR1Ked59ISLLlz4yLxNohKdBMuHKnN0odSaT0iZ0ie7ObmpjYh8+jjHUwdB04t89/1O/w1cDnyilFU=';
 
 
-echo 'sdfsasdf';
+echo 'sdfsavvvvsdf';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -33,7 +33,7 @@ if (!is_null($events['events'])) {
 					'text' => 'no'
 				];			
 				
-$vvv =  array($a1, $a2);
+$vvv =  [$a1, $a2];
 
 
 
