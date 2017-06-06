f5ca8baa83c3696a92f5873f6818c5e2559e5b1a
phoure <phoure.capuchino@msn.com>
2017-06-07 04:27:33 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 $access_token = '1n4HF8OIC9v65ocWyJAtnzMOUSyiZf6rrP1/xLKQDtFK+nKupweT4dVMBFP79mgVgC35CsJzx3pYOgRFBp7kodhi2d8/tXR1Ked59ISLLlz4yLxNohKdBMuHKnN0odSaT0iZ0ie7ObmpjYh8+jjHUwdB04t89/1O/w1cDnyilFU=';
 
 
-echo 'sdfsavvvvsdf';
+echo 'ddddddd';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -21,41 +21,21 @@ if (!is_null($events['events'])) {
 			$id = $event['source']['userId'];
 
 
-
-		$a1 = [
-					'type' => 'message',
-					'label' => 'Yes',
-					'text' => 'yes'
-				];
-	$a2 = [
-					'type' => 'message',
-					'label' => 'No',
-					'text' => 'no'
-				];			
-				
-$vvv =  [$a1, $a2];
-
-
-
-		$oo =[
-		'type' => 'confirm',
-			'text' => 'Are you sure?',
-			'actions' => [$vvv]
-			];
-			
-			
-	$mes = [
-		'type' => 'template',
-		'altText' => 'this is a confirm template',
-		'template' => [$oo]
-		]
-		
-		;
-		
-	$sss = array(
-	'replyToken' => $replyToken,
-	'messages' => [$mes]
+$aa = array([
+		'type' => 'text',
+		'text' => 'Are you sure?',
+	],
+	[
+		'type' => 'text',
+		'text' => 'sdfsfasdf',
+	]
 	);
+
+		$sss = array(
+	'replyToken' => $replyToken,
+	'messages' => [$aa]
+);
+
 					
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
