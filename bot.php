a5145ebe5bf0b19ccccc0822946cfa148182e644
phoure <phoure.capuchino@msn.com>
2017-06-07 02:42:52 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 $access_token = '1n4HF8OIC9v65ocWyJAtnzMOUSyiZf6rrP1/xLKQDtFK+nKupweT4dVMBFP79mgVgC35CsJzx3pYOgRFBp7kodhi2d8/tXR1Ked59ISLLlz4yLxNohKdBMuHKnN0odSaT0iZ0ie7ObmpjYh8+jjHUwdB04t89/1O/w1cDnyilFU=';
 
 
-echo '55565';
+echo '55+++++565';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -27,12 +27,13 @@ if (!is_null($events['events'])) {
 				'text' => $id
 			];
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
+$sss = [
 					'replyToken' => $replyToken,
 					'messages' => [$messages]
 					];
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [$sss];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
