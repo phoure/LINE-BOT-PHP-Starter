d79a787078b4dd1d1936fbd58cff4683c2d0bb58
phoure <phoure.capuchino@msn.com>
2017-06-07 03:25:53 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 $access_token = '1n4HF8OIC9v65ocWyJAtnzMOUSyiZf6rrP1/xLKQDtFK+nKupweT4dVMBFP79mgVgC35CsJzx3pYOgRFBp7kodhi2d8/tXR1Ked59ISLLlz4yLxNohKdBMuHKnN0odSaT0iZ0ie7ObmpjYh8+jjHUwdB04t89/1O/w1cDnyilFU=';
 
 
-echo 'vvveeeewwvvv';
+echo 'ssvvveessssseewwvvv';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -27,9 +27,12 @@ if (!is_null($events['events'])) {
 				'text' => $id
 			];
 
-	$sss = array(
+					$sss = array(
 						'replyToken' => $replyToken,
-						'type' => [$messages]
+						'messages' => [
+				'type' => 'text',
+				'text' => $id
+			]
 					);
 					
 			// Make a POST Request to Messaging API to reply to sender
