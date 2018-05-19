013019ccd4af7b66fe223a9be1d891009558c50d
phoure <phoure.capuchino@msn.com>
2018-05-19 23:48:53 +0700

    Update img_php.php

--- img_php.php
+++ img_php.php
@@ -1,7 +1,3 @@
 <?php
-  	$background = imagecreatefrompng("http://drivegay.com/drive/2016/upload/p6AJTJsfOfkIczo34PiC.png");
-
-			   header('Content-type: image/png');
-		echo 	imagepng($background);
-			//imagedestroy($background);
+  
 ?>
