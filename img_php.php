a6a6f06e2d48e3520e0da43706e549dfe5fb7ce8
phoure <phoure.capuchino@msn.com>
2018-05-19 23:46:09 +0700

    Create img_php.php

---
+++ img_php.php
@@ -0,0 +1,7 @@
+<?php
+  	$background = imagecreatefrompng("http://drivegay.com/drive/2016/upload/p6AJTJsfOfkIczo34PiC.png");
+
+			   header('Content-type: image/png');
+		echo 	imagepng($background);
+			//imagedestroy($background);
+?>
