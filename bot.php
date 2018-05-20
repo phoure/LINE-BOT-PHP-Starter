c4c9ea21bbad4c8ccd5eda91a33c53304da96dc8
phoure <phoure.capuchino@msn.com>
2018-05-20 13:23:30 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -214,14 +214,14 @@ if($_GET['post'] == '1'){
 	}
 		$strUrl = 'https://api.line.me/v2/bot/message/push';
 } 		
-
+/*
 		$strUrl = 'https://api.line.me/v2/bot/message/push';
 $data = array('to' => 'C214e858f2c0e42285b5d56a12f0cfced', 'messages' => array(
         [
           'type' => 'text',
           'text' => $textIn
         ]));
-
+*/
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
 curl_setopt($ch, CURLOPT_HEADER, false);
