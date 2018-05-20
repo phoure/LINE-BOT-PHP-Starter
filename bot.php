93757bcc9accde6b376df583e14dce537a66795e
phoure <phoure.capuchino@msn.com>
2018-05-20 07:11:17 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,14 +3,14 @@ $access_token = 'qNTXzzZpk6jEk57U46RK5iuyMyCjQRgF3GYrEyFOxBasHkdwuGeMMPdViCDbhvF
 
 
 $groupid[] = array('C214e858f2c0e42285b5d56a12f0cfced','test');
-/*
+
 $groupid[] = array('Cdcbc1ac3c747ec546fdd194c0fbf7b1f','clipgayzab');
 $groupid[] = array('C16bffe43b165df3429a722dde84adcfc','konrakphone');
 $groupid[] = array('C5acee5a1fea67f1e79201ded58d1f91d','gkawanrak');
 $groupid[] = array('C04ae8ed4e3d9f6e8de35cd48639b85c0','peodkkongwao1');
 $groupid[] = array('C19be33210e004052910aba5a817621e2','peodklongwao');
 $groupid[] = array('Cd08afe8945428db31485bca7effc88a2','mangkonnimitr');
-*/
+
 
 for ($x = 0; $x <= count($groupid)-1; $x++) {
 	
