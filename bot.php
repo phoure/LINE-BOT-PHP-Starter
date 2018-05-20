eea8bd3e4e5e38fdf12aa0df9a5a1e43cd89ffab
phoure <phoure.capuchino@msn.com>
2018-05-20 07:10:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,7 +17,7 @@ for ($x = 0; $x <= count($groupid)-1; $x++) {
 $card = array(
 	[
 	'type' => 'template',
-    "altText" => '🎬 แชร์วีดีโอ',
+    "altText" => '🎬 ไดรฟ์ได้แชร์วีดีโอ',
 	"template" => array(
 					'type' => 'buttons',
 					'thumbnailImageUrl' => $_GET['thumb'],
