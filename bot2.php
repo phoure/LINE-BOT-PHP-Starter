6176a2c78911c30fa7db41d07e0134fd79175c3b
phoure <phoure.capuchino@msn.com>
2018-05-20 09:14:57 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -3,14 +3,13 @@ $access_token = 'qNTXzzZpk6jEk57U46RK5iuyMyCjQRgF3GYrEyFOxBasHkdwuGeMMPdViCDbhvF
 
 if($_GET['post'] == '1'){
 	$groupid[] = array('C214e858f2c0e42285b5d56a12f0cfced','test');
-	/*
 	$groupid[] = array('Cdcbc1ac3c747ec546fdd194c0fbf7b1f','clipgaysab');
 	$groupid[] = array('C16bffe43b165df3429a722dde84adcfc','konrakphone');
 	$groupid[] = array('C5acee5a1fea67f1e79201ded58d1f91d','gkawanrak');
 	$groupid[] = array('C04ae8ed4e3d9f6e8de35cd48639b85c0','peodkkongwao1');
 	$groupid[] = array('C19be33210e004052910aba5a817621e2','peodklongwao');
 	$groupid[] = array('Cd08afe8945428db31485bca7effc88a2','mangkonnimitr');
-	*/
+	
 
 	$id[] = 'us6zcni'; $title[]= 'หมอกร อยากเย็ดจัง';  $thumb[] = 'https://pbs.twimg.com/media/DdmwFr9V0AAhjy0.jpg';
 	$id[] = '1JvpZrM'; $title[]= 'ชอบท่า หมอกรคงเสียวน่าดู หน้าอย่างเงี่ยนอะ';  $thumb[] = 'https://pbs.twimg.com/media/DdmwKmaU0AAD_pC.jpg';
