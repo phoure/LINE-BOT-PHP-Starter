60c727de75f16dd506abd8e6c620c8361f99bd33
phoure <phoure.capuchino@msn.com>
2018-05-20 09:35:28 +0700

    Add files via upload

---
+++ app_tokens.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+$consumer_key_vid = 'nvC6uQo2yaDSnVlCNmvhojqtO';
+$consumer_secret_vid = 's8ed0thvcTz2sD07wPt6WwxBDQ1Gcq36Y5duGEA3R2897pm5rG';
+$user_token_vid = '2999559512-Hsy3GQ3k5et1Vea2QbUuIgr3k6k5cX5Zbxurlm7';
+$user_secret_vid = 'eV9ApP9Nkp11drRXGwqM4PpN9qyo6ZAke9M66QpPVeAPd';
+
+$consumer_key_vid = 'pNySRYfW92QJCoGKaMxRtIZ2b';
+$consumer_secret_vid = 'GxLiptF96v3VAGQERxoj2V33niQ7MM40v1qvx1VojyK3x8P86f';
+$user_token_vid = '2999559512-tatV9GVo9PAKDc9wqnEeuQrzAJXG4gXC1vqKV7o';
+$user_secret_vid = 'oUeV2ldTrTN3ILVhd6bAIgKPqilISNITvffYouYApxrjy';
+
+
+
+$consumer_key = 'OdznijdRcVxq4Xz7yVuUIFlBC';
+$consumer_secret = 'dQh6tQAVJi7m4Mb9eT3dVQj1Qe13CWdVl6owlaIOSpvJxBMwh8';
+$user_token = '2999559512-r020yzgyk7v49ZTXuKvRhW6nxH8xlMwO1O5qLHl';
+$user_secret = 'Pq2sxWQuiKWZqbLpSIQLzYufrYcLLijtcyE58D3CAGysL';
+
+*/
+$consumer_key = 'Ku7V2dfb6bLxr9br3rdqgLbyw';
+$consumer_secret = 'EMTJcatG8Cc7qPofmGxsS9rZTI0aJAHAAc8We3hqLR1A8W9Lka';
+$user_token = '957508018052022273-TfAxiBe8ELJZicr8j0ayVnX1gV5sSD3';
+$user_secret = 'Utzltpv3hyZWuFYVW7Bz65Wot6IzFcs2ioBwRSn2bOTno';
+
+?>
