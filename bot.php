05777f8c123f0e9287b570d74fad1cb7886210b4
phoure <phoure.capuchino@msn.com>
2018-05-21 02:57:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -179,13 +179,12 @@ if($_GET['post'] == '1'){
 			$strUrl = 'https://api.line.me/v2/bot/message/push';
 	
 			$groupid[] = array('C214e858f2c0e42285b5d56a12f0cfced','test');
-	/*
 			$groupid[] = array('Cdcbc1ac3c747ec546fdd194c0fbf7b1f','clipgaysab');
 			$groupid[] = array('C16bffe43b165df3429a722dde84adcfc','konrakphone');
 			$groupid[] = array('C5acee5a1fea67f1e79201ded58d1f91d','gkawanrak');
 			$groupid[] = array('C04ae8ed4e3d9f6e8de35cd48639b85c0','peodkkongwao1');
 			$groupid[] = array('C19be33210e004052910aba5a817621e2','peodklongwao');
-			$groupid[] = array('Cd08afe8945428db31485bca7effc88a2','mangkonnimitr');*/
+			$groupid[] = array('Cd08afe8945428db31485bca7effc88a2','mangkonnimitr');
 	
 	if($_GET['target'] == 'text'){
 		for ($x = 0; $x <= count($groupid)-1; $x++) {
