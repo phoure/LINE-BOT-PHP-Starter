1f4bc78dd6a662998e5c61c77ec1797a8177a8e8
phoure <phoure.capuchino@msn.com>
2018-05-21 02:35:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -16,7 +16,7 @@ $nameIn = $arrJson['events'][0]['source']['userId'];
 if (strpos($textIn, 'twitter') !== false && $arrJson['events'][0]['source']['groupId'] == 'C214e858f2c0e42285b5d56a12f0cfced') {
 	
 	$data_vid = explode('http',$textIn);
-	$post_vid = file_get_contents('http://drivegay.com/docs/upload_file.php?action=upload_url&url=http'.$data_vid[1].'&caption='.str_replace(' ','-', $_GET['thumb']$data_vid[0]));
+	$post_vid = file_get_contents('http://drivegay.com/docs/upload_file.php?action=upload_url&url=http'.$data_vid[1].'&caption='.str_replace(' ','-', $data_vid[0]));
 	
 	$data = array(
 	'replyToken' => $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'],
