c16452985dfc542feed551bce067bbab6ca284c3
phoure <phoure.capuchino@msn.com>
2018-05-25 03:59:59 +0700

    Update bot3.php

--- bot3.php
+++ bot3.php
@@ -39,7 +39,7 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "image=".str_replace('../','http://drivegay.com/', str_replace('.png','_thumb.png', $_GET['thumb'])),
+  CURLOPT_POSTFIELDS => "image=".$response_data['pictureUrl'],
   CURLOPT_HTTPHEADER => array(
     "authorization: Client-ID 9247e4c204491c4",
     "cache-control: no-cache",
