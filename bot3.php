df86280e313182b595864a6659a9fdbc1443c996
phoure <phoure.capuchino@msn.com>
2018-05-25 04:17:36 +0700

    Update bot3.php

--- bot3.php
+++ bot3.php
@@ -1,6 +1,21 @@
 <?php
 $access_token = 'fgFWEdG3kij2u4EyyvvzhSPWLCfK8Z2duZ40kmpKgbiIn/HpV6bQe5UgHesnjOa07GizYWFnE0AoDjUnMLj4iEi/PufqScjhbj5QBxDDgeiHm82i9h5zLJ2y9kc0y7+1NlW9CUSbKsUU6X8KUQC26wdB04t89/1O/w1cDnyilFU=';
 
+$content = file_get_contents('php://input');
+$arrJson = json_decode($content, true);
+ 
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+
+
+$strUrl = "https://api.line.me/v2/bot/message/reply";
+  $arrPostData = array();
+ $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "join";
+
+
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
@@ -122,3 +137,15 @@ if($_GET['post'] == '1'){
 				echo $result . "\r\n";
 	} 
 }
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+ ?>
