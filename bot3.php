76a93040dd5546a845a5474b462d8002ad588e7b
phoure <phoure.capuchino@msn.com>
2018-05-25 05:19:23 +0700

    Update bot3.php

--- bot3.php
+++ bot3.php
@@ -4,82 +4,39 @@ $access_token = 'fgFWEdG3kij2u4EyyvvzhSPWLCfK8Z2duZ40kmpKgbiIn/HpV6bQe5UgHesnjOa
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
 
+$textIn = $arrJson['events'][0]['message']['text'];
+$nameIn = $arrJson['events'][0]['source']['userId'];
+
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$access_token}";
 
 
+if($textIn == 'ลงทะเบียน'){
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+	  CURLOPT_URL => "https://api.line.me/v2/bot/profile/".$arrJson['events'][0]['source']['userId'],
+	  CURLOPT_CUSTOMREQUEST => "GET",
+	  CURLOPT_RETURNTRANSFER => true,
+	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	  CURLOPT_HTTPHEADER => array(
+	    "authorization: Bearer ".$access_token,
+	    "postman-token: 7d55f84c-714f-e493-808f-c45ca4bcdfc5"
+	  ),
+	));
+	$response = curl_exec($curl);
+	$response_data = json_decode($response, true);
+	curl_close($curl);
 
+	 $strUrl = 'https://api.line.me/v2/bot/message/reply';
+ 	 $data = array( 'replyToken' => $arrJson['events'][0]['replyToken'],
+			'messages' => array([
+				'type' => 'text',
+				'text' =>  'ลงทะเบียนการแชร์ให้ '.$response_data['displayName'].' แล้ว'
+			]));
+	  send($data, $strUrl, $arrHeader);
 
-$curl = curl_init();
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.line.me/v2/bot/profile/".$arrJson['events'][0]['source']['userId'],
-  CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_HTTPHEADER => array(
-    "authorization: Bearer ".$access_token,
-    "postman-token: 7d55f84c-714f-e493-808f-c45ca4bcdfc5"
-  ),
-));
-
-$response = curl_exec($curl);
-$response_data = json_decode($response, true);
-curl_close($curl);
-
-$curl = curl_init();
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.imgur.com/3/image",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "image=".$response_data['pictureUrl'],
-  CURLOPT_HTTPHEADER => array(
-    "authorization: Client-ID 9247e4c204491c4",
-    "content-type: application/x-www-form-urlencoded",
-    "postman-token: 8b84fd63-b153-b627-403e-3c8251c250df"
-  ),
-));
-$response_img = curl_exec($curl);
-$response_img = json_decode($response_img,true);
-curl_close($curl);
-$response_img_url = $response_img['data']['link'];
-		
-	$groupid[] = array('C5ca58854e5e7ae33964770acadc0211d','test');
-
-	//$groupid[] = array('C214e858f2c0e42285b5d56a12f0cfced','drivegay');
-	for ($x = 0; $x <= count($groupid)-1; $x++) {
-	
-		$card = array(
-			     [
-			     'type' => 'template',
-				"altText" => '🎬 แชร์วีดีโอ',
-				"template" => array(
-				    'type' => 'buttons',
-				    'thumbnailImageUrl' => $response_img_url,
-				    'imageAspectRatio' => 'rectangle',
-				    'imageSize' => 'cover',
-				    'imageBackgroundColor' => '#000000',
-				    'text' => $response_data['displayName'],
-				    'defaultAction' =>  array(
-					 'type' => 'uri',
-					 'label' => 'ดูคลิปนี้',
-					 'uri' => 'http://drivegay.com/video/'.$_GET['id'].'&ref='.$groupid[$x][1]
-				    ),
-				    'actions' =>  array(['type' => 'uri',
-					 'label' => '🎬 ดูคลิปนี้',
-					 'uri' => 'http://drivegay.com/video/'.$_GET['id'].'&ref='.$groupid[$x][1]
-					])
-				    )
-				]
-				);
-			      
-
-				$strUrl = "https://api.line.me/v2/bot/message/reply";
-		
-				$data = array('replyToken' => $arrJson['events'][0]['replyToken'], 'messages' => $card);
-				send($data, $strUrl, $arrHeader);
-	}
+}
 
 function send($data, $strUrl, $arrHeader){
 	$ch = curl_init();
