e8c8c7f24f41317ccd62f04819453a9b1c5c264d
phoure <phoure.capuchino@msn.com>
2018-05-25 04:52:15 +0700

    Update bot3.php

--- bot3.php
+++ bot3.php
@@ -14,11 +14,6 @@ $arrHeader[] = "Authorization: Bearer {$access_token}";
 $curl = curl_init();
 curl_setopt_array($curl, array(
   CURLOPT_URL => "https://api.line.me/v2/bot/profile/".$arrJson['events'][0]['source']['userId'],
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "GET",
   CURLOPT_HTTPHEADER => array(
     "authorization: Bearer ".$access_token,
