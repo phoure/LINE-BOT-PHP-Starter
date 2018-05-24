a89d20ff217b2118b36c2d3b8b6cde80dbab347d
phoure <phoure.capuchino@msn.com>
2018-05-25 04:53:03 +0700

    Update bot3.php

--- bot3.php
+++ bot3.php
@@ -15,6 +15,8 @@ $curl = curl_init();
 curl_setopt_array($curl, array(
   CURLOPT_URL => "https://api.line.me/v2/bot/profile/".$arrJson['events'][0]['source']['userId'],
   CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_HTTPHEADER => array(
     "authorization: Bearer ".$access_token,
     "cache-control: no-cache",
