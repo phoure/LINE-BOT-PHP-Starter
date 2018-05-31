be449021fce6c4271489772ae196850f643a6f39
phoure <phoure.capuchino@msn.com>
2018-05-31 16:50:13 +0700

    Update bot3.php

--- bot3.php
+++ bot3.php
@@ -750,16 +750,7 @@ else if ($textIn == '.') {
 	
 	 $data = array('to' => 'C5ca58854e5e7ae33964770acadc0211d', 'messages' => $card);
 	 send($data, $strUrl, $arrHeader);
-	
-	
-	$strUrl = 'https://api.line.me/v2/bot/message/push';
-	$card = array([
-			'type' => 'text',
-			'text' => 'โพสงาน '.$arrJson['events'][0]['source']['userId']
-		      ]);
-	
-	 $data = array('to' => 'C5ca58854e5e7ae33964770acadc0211d', 'messages' => $card);
-	 send($data, $strUrl, $arrHeader);
+
 	
 }
 function send($data, $strUrl, $arrHeader){
