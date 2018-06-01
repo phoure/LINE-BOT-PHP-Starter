8b30ce13211d73d64371d25e8a4632910418ab4a
phoure <phoure.capuchino@msn.com>
2018-06-01 20:35:09 +0700

    Update game.php

--- game.php
+++ game.php
@@ -1,7 +1,16 @@
 <?php
-    $access_token = 'hfdfyAmqI79pFXL+upQ6LobXR6TRwi9ydmcwYuhgLgyg/1Vx8EckOUbGxo41Bt4oN38CW4hgI82pB2VX3Psttb6X3Kz50/Kq4TPjpJNzfhPMSUrhhr5xRMyhapAbU00orb6TFddfVy2VvbrnBJqEgAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'hfdfyAmqI79pFXL+upQ6LobXR6TRwi9ydmcwYuhgLgyg/1Vx8EckOUbGxo41Bt4oN38CW4hgI82pB2VX3Psttb6X3Kz50/Kq4TPjpJNzfhPMSUrhhr5xRMyhapAbU00orb6TFddfVy2VvbrnBJqEgAdB04t89/1O/w1cDnyilFU=';
+$content = file_get_contents('php://input');
+$arrJson = json_decode($content, true);
+$textIn = $arrJson['events'][0]['message']['text'];
+$idIn = $arrJson['events'][0]['source']['userId'];
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$access_token}";
 
-
+	 $strUrl = 'https://api.line.me/v2/bot/message/reply';
+ 	 $data = array( 'replyToken' => $arrJson['events'][0]['replyToken'], 'type' => 'join');
+	 send($data, $strUrl, $arrHeader);
 
 
 
