4f2ac1f97bf21b030d2025c605e17ed39a480150
phoure <phoure.capuchino@msn.com>
2018-06-01 21:39:55 +0700

    Update game.php

--- game.php
+++ game.php
@@ -3,13 +3,10 @@ $access_token = 'hfdfyAmqI79pFXL+upQ6LobXR6TRwi9ydmcwYuhgLgyg/1Vx8EckOUbGxo41Bt4
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
 $textIn = $arrJson['events'][0]['message']['text'];
+$replayId = $arrJson['events'][0]['replyToken'];
 $idIn = $arrJson['events'][0]['source']['userId'];
 $group = 'Cd3f6dbdec8b434e7a2c6db4997b5769d';
-/*
-	 $strUrl = 'https://api.line.me/v2/bot/message/reply';
- 	 $data = array( 'replyToken' => $arrJson['events'][0]['replyToken'], 'type' => 'join');
-	 send($data, $strUrl, $arrHeader);
-*/
+
 if ($textIn == '.') {
 	
 	$card = array([
@@ -17,27 +14,14 @@ if ($textIn == '.') {
 			'text' => 'กลุ่ม ID '.$arrJson['events'][0]['source']['groupId']
 		      ]);
 	
-	 send($card,  $arrJson['events'][0]['replyToken']);
+	 send($card, 'reply');
 
 	
 }
-/*
 
-else if ($_GET['action' == 'calc') {
-	
-	$strUrl = 'https://api.line.me/v2/bot/message/reply';
-	$card = array([
-			'type' => 'text',
-			'text' => 'กลุ่ม ID '.$arrJson['events'][0]['source']['groupId']
-		      ]);
-	
-	 send($card, $strUrl, $arrHeader);
-
-	
-}
-*/
 		
-function send($card,$to){
+function send($card,$target ,$to){
+	
 	
 	$access_token = 'hfdfyAmqI79pFXL+upQ6LobXR6TRwi9ydmcwYuhgLgyg/1Vx8EckOUbGxo41Bt4oN38CW4hgI82pB2VX3Psttb6X3Kz50/Kq4TPjpJNzfhPMSUrhhr5xRMyhapAbU00orb6TFddfVy2VvbrnBJqEgAdB04t89/1O/w1cDnyilFU=';
 
@@ -45,10 +29,15 @@ function send($card,$to){
 	$arrHeader[] = "Content-Type: application/json";
 	$arrHeader[] = "Authorization: Bearer {$access_token}";
 	
-	$strUrl = 'https://api.line.me/v2/bot/message/reply';
-
 	
-	$data = array('replyToken' => $to, 'messages' => $card);
+	if($target == 'replay'){
+		$strUrl = 'https://api.line.me/v2/bot/message/reply';
+		$data = array('replyToken' => $to, 'messages' => $card);
+	}
+	else if($target == 'push'){
+		$strUrl = 'https://api.line.me/v2/bot/message/push';
+		$data = array('to' => $to, 'messages' => $card);
+	}
 	
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL,$strUrl);
