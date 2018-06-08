d82e5037973c5baede2a1b38a3e32b3f3dcc1c42
phoure <phoure.capuchino@msn.com>
2018-06-08 12:16:35 +0700

    Update bot4.php

--- bot4.php
+++ bot4.php
@@ -40,7 +40,7 @@ if($_GET['post'] == '1'){
 	$groupid[] = array('C04ae8ed4e3d9f6e8de35cd48639b85c0','peodkkongwao1');
 		
 	$groupid[] = array('Cdcbc1ac3c747ec546fdd194c0fbf7b1f','clipgaysab');
-*/
+
 	$id[] = 'เทคนิคและวิธีโพสต์สินค้าในไลน์ให้ขายดี 2018'; $title[]= 'สอนโปรโมท โพสต์โฆษณาอย่างไรให้เด่น สะดุดตา แบบมืออาชีพ';  $thumb[] = 'https://www.img.in.th/images/3d4ed988b8d35b0517a01b7a8152db17.jpg';
 	$id[] = 'โพสต์โฆษณาให้เด่นและปังกว่าใคร'; $title[]= 'โพสโฆษณาด้วยรูปแบบต่างๆ ที่ใครเห็นก็ต้องกดดู'; $thumb[] = 'https://www.img.in.th/images/f2de649746267a9556855833d96f357a.jpg';
 	$id[] = 'ส่ง Rich Message ได้ฟรีๆ'; $title[]= 'ส่งข้อความภาพแบบเต็มจอโดยไม่ต้องเสียเงินสักบาท'; $thumb[] = 'https://www.img.in.th/images/3305912fa6e44c8246dcc9f5e0c0beee.jpg';
