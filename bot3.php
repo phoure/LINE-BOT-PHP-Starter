3272a9b81b5c52a54ca870229112c1754991aade
phoure <phoure.capuchino@msn.com>
2018-06-10 19:27:05 +0700

    Update bot3.php

--- bot3.php
+++ bot3.php
@@ -39,17 +39,6 @@ if($arrJson['events'][0]['source']['groupId'] == ''){
 	
 	
 	
-	$strUrl = 'https://api.line.me/v2/bot/message/push';
-	$card = array([
-					'type' => 'text',
-					'text' => $arrJson['events'][0]['source']['userId']." : ".$textIn
-				      ])
-	  					  )
-	]
-	);
-	
-	 $data = array('to' => 'C5ca58854e5e7ae33964770acadc0211d', 'messages' => $card);
-	 send($data, $strUrl, $arrHeader);
 
 }
 
