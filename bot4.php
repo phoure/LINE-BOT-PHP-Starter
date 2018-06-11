8cb5cdf8ba2332fecf4c6f434bfaa4f36d037042
phoure <phoure.capuchino@msn.com>
2018-06-11 21:49:35 +0700

    Update bot4.php

--- bot4.php
+++ bot4.php
@@ -3,7 +3,7 @@ $access_token = 'kimeIkhXon3CjV2oBQDdvzI15V7Fe0eIt7gvjAVghuSyvAkhQYJ+fb/M4XQdLe/
 if($_GET['post'] == '1'){
 	//$groupid[] = array('Cca7d5d4fa77312c96f6fc6cb452d649e','sleep');
 
-	$groupid[] = array('C5ca58854e5e7ae33964770acadc0211d','test');
+//	$groupid[] = array('C5ca58854e5e7ae33964770acadc0211d','test');
 
 	$groupid[] = array('C8900370d1a454dc66aec73dd6e338e2e','test');
 	$groupid[] = array('C7299d5f55789ad1c3289b9349d7f7979','test');
