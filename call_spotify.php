a1c1283d551b3eff64f101fdafc23d1d2132b15b
phoureo <phoure.capuchino@msn.com>
2018-12-23 08:05:28 +0700

    Update call_spotify.php

--- call_spotify.php
+++ call_spotify.php
@@ -42,7 +42,7 @@ $curl = curl_init();
 }
 
 
-$c_artist = curl('https://api.spotify.com/v1/artists/5pokGZ1K9Hr6etaKPDxSG8/albums?oauth_signature_method=HMAC-SHA1&oauth_timestamp=1545369076&oauth_nonce=bg7JCU&oauth_version=1.0&oauth_signature=D4g1si4f4npcYqeUJTR5YuNB1Cg%3D&market=TH&limit=50&offset=0');
+$c_artist = curl('https://api.spotify.com/v1/artists/5pokGZ1K9Hr6etaKPDxSG8&market=TH&limit=50&offset=0');
 
 for ($x = 0; $x <= count($c_artist['items'])-1; $x++) {
 	$c_album = curl('https://api.spotify.com/v1/albums/'.$c_artist['items'][$x]['id'].'/tracks?market=TH&limit=50&offset=0');
