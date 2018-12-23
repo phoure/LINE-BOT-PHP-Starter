38641a63d282bd56513fbc6841a58c40ec01aeda
phoureo <phoure.capuchino@msn.com>
2018-12-23 07:24:26 +0700

    Update call_spotify.php

--- call_spotify.php
+++ call_spotify.php
@@ -50,7 +50,7 @@ for ($x = 0; $x <= count($c_artist['items']); $x++) {
 		$song[] = $c_album['items'][$x]['name'];
 	}
 	$songs[] = join('---',$song);
-	$song = '';
+	unset($song);
 }
 
 	$songss = join('<br /><br /><br />',$songs);
