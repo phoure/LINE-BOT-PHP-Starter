4b4048183cb77f951e5cab553dabdf5c73101516
phoureo <phoure.capuchino@msn.com>
2018-12-23 08:00:14 +0700

    Update call_spotify.php

--- call_spotify.php
+++ call_spotify.php
@@ -47,7 +47,7 @@ $c_artist = curl('https://api.spotify.com/v1/artists/5pokGZ1K9Hr6etaKPDxSG8/albu
 for ($x = 0; $x <= count($c_artist['items']); $x++) {
 	$c_album = curl('https://api.spotify.com/v1/albums/'.$c_artist['items'][$x]['id'].'/tracks?market=TH&limit=50&offset=0');
 	for ($s = 0; $s <= count($c_album['items']); $s++) {
-		$song[] = $c_album['items'][$x]['name'];
+		$song[] = $c_album['items'][$s]['name'];
 	}
 	$songs[] = join('---',$song);
 	unset($song);
