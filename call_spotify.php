15fff1115e3ef0de2db870523959f92b8b76cfb1
phoureo <phoure.capuchino@msn.com>
2018-12-23 09:16:47 +0700

    Update call_spotify.php

--- call_spotify.php
+++ call_spotify.php
@@ -1,5 +1,3 @@
-<?php
-
 function debug($var){	
      // หาที่มาและบรรทัดของไฟล์ที่เรียกใช้ฟังก์ชัน debug 
      $trace = reset(debug_backtrace());	
@@ -45,8 +43,9 @@ $curl = curl_init();
 $c_artist = curl('https://api.spotify.com/v1/artists/5pokGZ1K9Hr6etaKPDxSG8/albums?include_groups=album%2Csingle&market=TH&limit=50&offset=0');
 
 for ($x = 0; $x <= count($c_artist['items'])-1; $x++) {
-
-	$album[] = $c_artist['items'][$x]['id'].'///'.$c_artist['items'][$x]['album_type'].'///'.$c_artist['items'][$x]['images'][0]['url'].'++'.$c_artist['items'][$x]['images'][1]['url'].'++'.$c_artist['items'][$x]['images'][2]['url'].'///'.$c_artist['items'][$x]['release_date'];
+	
+	$al_name = $c_artist['items'][$x]['name'];
+	$album[] = $c_artist['items'][$x]['id'].'///'.$al_name.'///'.$c_artist['items'][$x]['album_type'].'///'.$c_artist['items'][$x]['images'][0]['url'].'++'.$c_artist['items'][$x]['images'][1]['url'].'++'.$c_artist['items'][$x]['images'][2]['url'].'///'.$c_artist['items'][$x]['release_date'];
 
 	$c_album = curl('https://api.spotify.com/v1/albums/'.$c_artist['items'][$x]['id'].'/tracks?market=TH&limit=50&offset=0');
 	for ($s = 0; $s <= count($c_album['items'])-1; $s++) {
@@ -58,6 +57,7 @@ for ($x = 0; $x <= count($c_artist['items'])-1; $x++) {
 
 		$so[] = $c_album['items'][$s]['id'];
 		$so[] = $c_album['items'][$s]['name'];
+		$so[] = $al_name;
 		$so[] = $c_album['items'][$s]['track_number'];
 		$so[] = join('+', $ar);
 		$so[] = $c_album['items'][$s]['duration_ms'];
