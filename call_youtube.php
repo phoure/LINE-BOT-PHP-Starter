6971770bb51bdd6d2281eed25ad3f7f6949db87c
phoureo <phoure.capuchino@msn.com>
2018-12-23 20:02:10 +0700

    Add files via upload

---
+++ call_youtube.php
@@ -0,0 +1,41 @@
+<?php
+
+
+function debug($var){	
+     $trace = reset(debug_backtrace());	
+     $trace['file'] = str_replace(str_replace('/','\\',$_SERVER['DOCUMENT_ROOT']).'\\','',$trace['file']);	
+     echo "<pre>";
+     print_r($var);
+     echo "</pre>";
+     return $var;	
+}
+
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://www.googleapis.com/youtube/v3/search/?part=snippet&maxResults=25&q=surfing&key=AIzaSyAP2kHL9vib5QKuGsoKf8oBnUEH7HerFME",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache",
+    "postman-token: f088b721-0206-296c-8871-b708f6e05e0c"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo debug($response);
+}
+
+?>
